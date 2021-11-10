6be3a9bce43d08a0f22478acccfd390a214963fe
Pier Guinzani <pier_holanda@hotmail.com>
2021-11-09 22:06:06 -0300

    add env config

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ const wss = new Server({ server });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
   ws.on('close', () => console.log('Client disconnected'));
+
 });
 
 setInterval(() => {
